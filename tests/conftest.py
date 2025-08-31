91c75a27c545a36056cc61d362d92826e0303a99
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-31 10:43:43 +0530

    test: add configtest file

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+from authmint.services import TokenMint
+from authmint.settings import Settings
+from pytest import fixture
+from dotenv import load_dotenv
+
+# Load all env variables.
+load_dotenv()
+
+
+@fixture(scope="session")
+def email_verification_mint() -> TokenMint:
+    """Create token mint instance and return it."""
+    return TokenMint(
+        settings=Settings(
+            issuer="mytest.service",
+            audience="user",
+            purpose="email-verification",
+            expiry_duration=timedelta(minutes=5),
+            prevent_replay=True,
+        )
+    )
