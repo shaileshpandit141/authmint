9f169f59e12fe7fa613d2e3208508e5eb7aa1394
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-09-07 12:16:47 +0530

    test: add more test case

--- tests/services/test_token_mint.py
+++ tests/services/test_token_mint.py
@@ -1,4 +1,5 @@
 from authmint.services import TokenMint
+from pytest import raises
 from authmint.exceptions import InvalidTokenError
 
 
@@ -10,25 +11,35 @@ def test_token_mint_instance(email_verification_mint: TokenMint) -> None:
 def test_generated_token(email_verification_mint: TokenMint) -> None:
     """Generated a email verification token."""
     token = email_verification_mint.generate_token(
-        subject_id="user:25",
+        subject="user@gmail.com",
         extra_claims={"role": "admin"},
     )
 
     assert isinstance(token, str)
 
 
-def test_validate_generated_token(email_verification_mint: TokenMint) -> None:
+def test_validate_valid_token(email_verification_mint: TokenMint) -> None:
     """Validate Generated token."""
     token = email_verification_mint.generate_token(
-        subject_id="user:25",
+        subject="user@gmail.com",
         extra_claims={"role": "admin"},
     )
 
-    assert isinstance(token, str)
-    try:
-        claim = email_verification_mint.validate_token(
-            token=token,
-        )
-        assert isinstance(claim, dict)
-    except InvalidTokenError:
-        pass
+    claims = email_verification_mint.validate_token(token=token)
+
+    assert isinstance(claims, dict)
+    assert claims["sub"] == "user@gmail.com"
+    assert claims["ext"]["role"] == "admin"
+
+
+def test_validate_invalid_token(email_verification_mint: TokenMint) -> None:
+    """Validate Generated token."""
+    token = email_verification_mint.generate_token(
+        subject="user@gmail.com",
+        extra_claims={"role": "admin"},
+    )
+
+    email_verification_mint.revoke_token(token=token)
+
+    with raises(InvalidTokenError):
+        email_verification_mint.validate_token(token=token)
